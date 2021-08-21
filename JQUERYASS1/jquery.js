048603773f83be9674a6a32e2ae0f3d37356e2fb
Daniela Moreno <68881261+danielamoreno699@users.noreply.github.com>
2021-08-21 18:53:30 -0500

    JQUERYASS1

---
+++ JQUERYASS1/jquery.js
@@ -0,0 +1,30 @@
+$(document).ready(function(){
+
+    $('.test').text('ready!!!').css('color', 'green').fadeIn
+
+    $("a[href= 'facebook.com']").css('color', 'pink');
+
+    $("button").click(function(){
+        $("#item-list").find("a").css('color', 'red');
+        $("#item-list > li:first").css('color', 'yellow');
+        
+
+       
+
+
+
+        
+
+
+
+
+
+
+    }
+    
+    )
+
+
+}
+)
+
